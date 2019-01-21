65920cef1d5700f0f651742bd3d18dc9a06b3628
Juan Pablo Ramirez Giraldo - Ceiba Software <juan.giraldo@ceiba.com.co>
2019-01-21 07:24:36 -0500

    Se corrige prueba unitaria

--- src/test/java/co/com/ceiba/estacionamiento/juan/giraldo/unitaria/aplicacion/reglasnegocio/ingreso/ValidadorIngresoDiasTest.java
+++ src/test/java/co/com/ceiba/estacionamiento/juan/giraldo/unitaria/aplicacion/reglasnegocio/ingreso/ValidadorIngresoDiasTest.java
@@ -2,6 +2,9 @@ package co.com.ceiba.estacionamiento.juan.giraldo.unitaria.aplicacion.reglasnego
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Test;
 
 import co.com.ceiba.estacionamiento.juan.giraldo.aplicacion.entidad.vehiculo.Vehiculo;
@@ -34,11 +37,20 @@ public class ValidadorIngresoDiasTest {
 		ValidadorIngresoDias validadorIngresoDias = new ValidadorIngresoDias();		
 		Vehiculo vehiculo = new VehiculoDataBuilder().setPlaca(placaRestringida).buildVehiculo();
 		
+		Date fechaActual = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(fechaActual);
+		boolean ingresoValido = false;
+		
+		// Si es domingo o lunes permite Ingreso 
+		if ( calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ) ingresoValido = true;		
+		if ( calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY ) ingresoValido = true;
+		
 		// Act
-		boolean ingresoValido = validadorIngresoDias.validarIngreso(vehiculo);
+		boolean ingresoValidoActual = validadorIngresoDias.validarIngreso(vehiculo);
 
 		// Assert
-		assertFalse( ingresoValido );
+		assertTrue( ingresoValido == ingresoValidoActual );
 
 	}
 
